2d1b272f54f4e3caee8ae4a8d20c39d346721cdb
sona-user-369 <donadavak@gmail.com>
2025-03-15 23:47:31 +0100

    add settings changing operations

---
+++ django_startup/core/operations.py
@@ -0,0 +1,95 @@
+import ast
+import os
+import re
+
+
+def change_variable_value(variable_name, value: [str, list], filename, type_variable='str', with_comma=True):
+    if type_variable == '':
+        raise ValueError('Type of variable must variable')
+
+    if type_variable not in ['str', 'list', 'dict']:
+        raise ValueError('type_variable must be str or list or dict')
+
+    if not with_comma:
+        change_str_variable_value(variable_name, value, filename, with_comma=False)
+    elif type_variable == 'str':
+        change_str_variable_value(variable_name, value, filename)
+    elif type_variable == 'list':
+        change_list_variable_value(variable_name, value, filename)
+
+
+def change_str_variable_value(variable_name, value, filename, with_comma=True):
+    with open(filename, "r") as f:
+        content = f.read()
+
+    # Match: variable_name = "old_value" (handling spaces & comments)
+    pattern = rf"^\s*{variable_name}\s*=\s*'.*?'"
+    replacement = f'{variable_name} = "{value}"' if with_comma else f'{variable_name} = {value}'
+
+    if re.search(pattern, content, re.MULTILINE):
+        modified_content = re.sub(pattern, replacement, content, flags=re.MULTILINE)
+    else:
+        # If the variable doesn't exist, append it at the end
+        modified_content = content + f'\n{replacement}\n'
+
+    with open(filename, "w") as f:
+        f.write(modified_content)
+
+
+def create_env_file(folder_path, env_vars):
+    """
+    Creates a .env file in the specified folder and writes environment variables to it.
+
+    :param folder_path: The directory where .env should be created.
+    :param env_vars: A dictionary of environment variables to write.
+    """
+    # Ensure the folder exists
+    os.makedirs(folder_path, exist_ok=True)
+
+    # Define the file path
+    env_file_path = os.path.join(folder_path, ".env")
+
+    # Write environment variables to the file
+    with open(env_file_path, "w") as f:
+        for key, value in env_vars.items():
+            f.write(f"{key}={value}\n")
+
+    print(f".env file created successfully at {env_file_path}")
+
+
+def add_variable_value(variable_name, value, filename):
+    """
+    Add a variable to the file with the specified value
+    :param variable_name: The name of the variable to add the file.
+    :param value: A value of variable.
+    :param filename: The path of the file to modifying
+    """
+    with open(filename, "r") as f:
+        content = f.readlines()
+
+    content.append(f"{variable_name} = {value}\n")
+
+    with open(filename, "w") as f:
+        f.writelines(content)
+
+
+def change_list_variable_value(variable_name, value: [str, list], filename):
+    with open(filename, "r") as f:
+        content = f.read()
+
+    # Trouver la liste actuelle
+    match = re.search(rf'{variable_name}\s*=\s*(\[[^\]]*\])', content, re.MULTILINE)
+    if match:
+        actual_list = ast.literal_eval(match.group(1))  # Convertir en liste Python
+        if isinstance(value, str):
+            if value not in actual_list:
+                actual_list.append(value)
+
+        if isinstance(value, list):
+            actual_list = actual_list + value
+
+        # Replace file content
+        content_modified = content.replace(match.group(1), str(actual_list))
+
+        with open(filename, "w") as f:
+            f.write(content_modified)
--- django_startup/core/utils.py
+++ django_startup/core/utils.py
@@ -1,7 +1,13 @@
+import secrets
+import string
 import subprocess
 
 import re
 import os
+
+import click
+
+from django_startup.core.operations import change_variable_value, create_env_file
 
 
 def generate_env_name(project_name):
@@ -35,16 +41,75 @@ def install_env(project_name):
     # create and activate environment
     subprocess.check_call(["python3", "-m" "venv", env_path])
 
+    # modify settings
+    modify_settings(project_name)
+
+    # add .env file
+
     django_package = "django"
     django_rest_package = "django-rest-framework"
+    decouple_package = "python-decouple"
     django_startup_package = "django-startup"
 
-    subprocess.check_call([python_executable, "-m", "pip", "install", django_package, django_rest_package])
+    subprocess.check_call([
+        python_executable, "-m", "pip", "install",
+        django_package,
+        django_rest_package,
+        decouple_package
+    ])
 
 
 def create_project_directory(name):
+    """Create the project with django-admin startproject."""
     try:
         subprocess.check_call(["django-admin", "startproject", f"{name}"])
     except SystemExit:
         subprocess.check_call(["pip", "install", "django"])
         create_project_directory(name)
+
+
+def generate_secret_key(length=50):
+    """Generate a random Django SECRET_KEY."""
+    chars = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(secrets.choice(chars) for _ in range(length))
+
+
+def modify_settings(project_name):
+    """Modify settings.py file and set .env file for settings variables such as
+    SECRET_KEY, DEBUG, ALLOWED_HOSTS, INSTALLED_APPS, AUTH_USER_MODEL,
+    STATIC_ROOT, and STATIC_URL, STATICFILES_DIRS, LOGGING. """
+    # get settings file path
+    file_path = os.path.join(project_name, project_name, "settings.py")
+    click.echo(f"Modifying settings file at {file_path}")
+
+    """Import decouple package for get variables from .env file"""
+
+    with open(file_path, "r") as f:
+        content = f.readlines()
+
+    content.insert(0, "from decouple import config\n")
+
+    with open(file_path, "w") as f:
+        f.writelines(content)
+
+    env_data = {
+        "SECRET_KEY": generate_secret_key(),
+        "DEBUG": "True",
+        "ALLOWED_HOSTS": ["*"]
+    }
+
+    create_env_file(project_name, env_data)
+
+    # Change SECRET_KEY
+    change_variable_value("SECRET_KEY", "config('SECRET_KEY')", file_path, with_comma=False)
+    # Change DEBUG
+    change_variable_value("DEBUG", "config('DEBUG')", file_path, with_comma=False)
+    # Change ALLOWED_HOSTS
+    change_variable_value("ALLOWED_HOSTS", "config('ALLOWED_HOSTS')", file_path, with_comma=False)
+    # Change INSTALLED_APPS
+    change_variable_value("INSTALLED_APPS", ["rest_framework", "rest_framework.authtoken"] ,
+                          file_path, type_variable='list')
+
+
+def create_authentication_app():
+    pass
